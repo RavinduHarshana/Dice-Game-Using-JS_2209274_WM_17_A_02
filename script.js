879a2dca24f5d3b5f814317458ad976c76f64a66
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-11-14 07:36:45 +0530

    change background image

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
 function setname() {
   p1name = document.getElementById("P1Name").value;
   p2name = document.getElementById("P2Name").value;
-if(p1name=="" || p2name==""){
-  alert("Please enter Player Name");
-  breakp();
-}
-
+  if (p1name == "" || p2name == "") {
+    alert("Please enter Player Name");
+    breakp();
+  }
 
   document.getElementById("P1N").innerHTML = p1name;
   document.getElementById("P2N").innerHTML = p2name;
@@ -17,47 +15,44 @@ if(p1name=="" || p2name==""){
   document.getElementById("namei").style.display = "None";
 }
 
-marks1=0;
-marks2=0;
+marks1 = 0;
+marks2 = 0;
 
-function roldice(){
+function roldice() {
   p1name = document.getElementById("P1Name").value;
   p2name = document.getElementById("P2Name").value;
-    randnum1=Math.floor(Math.random()*6)+1;
-    randnum2=Math.floor(Math.random()*6)+1;
-    console.log(randnum1);
-    console.log(randnum2);
+  randnum1 = Math.floor(Math.random() * 6) + 1;
+  randnum2 = Math.floor(Math.random() * 6) + 1;
+  console.log(randnum1);
+  console.log(randnum2);
 
-    dice1=document.getElementById("p1i");
-    dice2=document.getElementById("p2i");
+  dice1 = document.getElementById("p1i");
+  dice2 = document.getElementById("p2i");
 
-    dice1.setAttribute('src','./images/dice'+randnum1+'.png');
-    dice2.setAttribute('src','./images/dice'+randnum2+'.png');
+  dice1.setAttribute("src", "./images/dice" + randnum1 + ".png");
+  dice2.setAttribute("src", "./images/dice" + randnum2 + ".png");
 
-    state="";
+  state = "";
 
-    if(randnum1>randnum2){
-      state=p1name+" Win";
-    }
-    else if(randnum2>randnum1){
-      state=p2name+" Win";
-    }
-    else{
-      state="Draw";
-    }
-    document.getElementById("Head").innerHTML=state;
+  if (randnum1 > randnum2) {
+    state = p1name + " Win";
+  } else if (randnum2 > randnum1) {
+    state = p2name + " Win";
+  } else {
+    state = "Draw";
+  }
+  document.getElementById("Head").innerHTML = state;
 
-    m1=marks1+=randnum1;
-    m2=marks2+=randnum2;
+  m1 = marks1 += randnum1;
+  m2 = marks2 += randnum2;
 
-    document.getElementById("P1marks").innerHTML=m1;
-    document.getElementById("P2marks").innerHTML=m2;
+  document.getElementById("P1marks").innerHTML = m1;
+  document.getElementById("P2marks").innerHTML = m2;
 
-    document.getElementById("marks").style.display="block"
+  document.getElementById("marks").style.display = "block";
 }
 
-function reset(){
-
+function reset() {
   document.getElementById("P1N").innerHTML = "";
   document.getElementById("P2N").innerHTML = "";
 
